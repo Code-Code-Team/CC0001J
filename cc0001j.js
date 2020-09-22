f445b2c255f7a92b3b4c90ae39ef9d21e5374e76
E2029-Aydin <aydinsanoz2@gmail.com>
2020-09-22 16:25:10 +0300

    fibo function added

---
+++ cc0001j.js
@@ -0,0 +1,42 @@
+
+
+document.getElementById("check").addEventListener("click",function(){
+
+    const n = document.getElementById("index").value;
+    document.getElementById("index").value=""
+    document.getElementById("index").focus()
+    
+    
+    console.log("fibonacci(n)", fibonacci(n))
+    
+    document.getElementById("indexNum").innerHTML = "Index :" + n;
+    document.getElementById("result").innerHTML= "fibonacci(n)= " + fibonacci(n);
+})
+
+document.getElementById("index").addEventListener("keyup",function(event){
+    if(event.which === 13){
+        const n = document.getElementById("index").value;
+        document.getElementById("index").value=""
+        document.getElementById("index").focus()
+        
+        console.log("fibonacci(n)", fibonacci(n))
+        document.getElementById("indexNum").innerHTML = "Index :" + n;
+        document.getElementById("result").innerHTML= "fibo(n)= " + fibonacci(n);
+
+    }
+})
+
+
+function fibonacci(n){
+    arr = [1,1];
+    for(let i = 0; i < n; i++ ){
+        arr.push(arr[i]+arr[i+1]);   
+    }
+    return arr[n];
+}
+
+
+
+
+
+
