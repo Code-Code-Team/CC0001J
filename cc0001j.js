4e3ac5c37fbee9f04bcd4cdd58265dac5a2eaef3
E2029-Aydin <aydinsanoz2@gmail.com>
2020-09-25 13:47:38 +0300

    feat:add myOperation() func

--- cc0001j.js
+++ cc0001j.js
@@ -2,37 +2,34 @@
 
 document.getElementById("check").addEventListener("click",function(){
 
-    const n = document.getElementById("index").value;
-    document.getElementById("index").value=""
-    document.getElementById("index").focus()
-    
-    
-    console.log("fibonacci(n)", fibonacci(n))
-    
-    document.getElementById("indexNum").innerHTML = "Index :" + n;
-    document.getElementById("result").innerHTML= "fibo(n)= " + fibonacci(n);
+    myOperation()
 })
 
 document.getElementById("index").addEventListener("keyup",function(event){
     if(event.which === 13){
-        const n = document.getElementById("index").value;
-        document.getElementById("index").value=""
-        document.getElementById("index").focus()
-        
-        console.log("fibonacci(n)", fibonacci(n))
-        document.getElementById("indexNum").innerHTML = "Index :" + n;
-        document.getElementById("result").innerHTML= "fibo(n)= " + fibonacci(n);
-
+        myOperation()
     }
 })
 
+function myOperation(){
+    const n = document.getElementById("index").value;
+    document.getElementById("index").value=""
+    document.getElementById("index").focus()
+    console.log("fibonacci(n)", fibonacci(n))
+    document.getElementById("indexNum").innerHTML = "Index :" + n;
+    document.getElementById("result").innerHTML= "fibo(n)= " + fibonacci(n);
 
+}
 function fibonacci(n){
-    arr = [1,1];
-    for(let i = 0; i < n; i++ ){
-        arr.push(arr[i]+arr[i+1]);   
+    if(n > 0){ 
+        arr = [1,1];
+        for(let i = 0; i < n; i++ ){
+            arr.push(arr[i]+arr[i+1]);   
+        }
+        return arr[n];
+    }else{
+        return alert('Enter a positive number !')
     }
-    return arr[n];
 }
 
 
